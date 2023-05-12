7656347cd063fc2599e19467788cbb0e674808b3
Azura ML <lasai.azura.maya@gmail.com>
2023-05-12 13:37:29 -0700

    fixing bugs

--- index.js
+++ index.js
@@ -375,35 +375,35 @@ app.post('/loggingin', async (req,res) => {
   
 
 
-app.get("/resources", (req, res) => {
-	fs.readFile(path.join(__dirname, 'app/campy.json'), 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(500, {'Content-Type': 'text/plain'});
-      res.end('Internal Server Error');
-      return;
-    }
+// app.get("/resources", (req, res) => {
+// 	fs.readFile(path.join(__dirname, 'app/campy.json'), 'utf8', (err, data) => {
+//     if (err) {
+//       res.writeHead(500, {'Content-Type': 'text/plain'});
+//       res.end('Internal Server Error');
+//       return;
+//     }
 
-    // Parse the JSON data
-    const jsonData = JSON.parse(data);
+//     // Parse the JSON data
+//     const jsonData = JSON.parse(data);
 
-    // Set the response headers
-    // res.writeHead(200, {'Content-Type': 'text/html'});
+//     // Set the response headers
+//     // res.writeHead(200, {'Content-Type': 'text/html'});
 
-    // Create the HTML list
-    let htmlList = '<ul>';
-    jsonData.forEach((item) => {
-      htmlList += `<li><a href="${item.link}">${item.title}</a>`;
-      if (item.ed) {
-        htmlList += `<span>${item.ed}</span>`;
-      }
-      htmlList += '</li>';
-    });
-    htmlList += '</ul>';
+//     // Create the HTML list
+//     let htmlList = '<ul>';
+//     jsonData.forEach((item) => {
+//       htmlList += `<li><a href="${item.link}">${item.title}</a>`;
+//       if (item.ed) {
+//         htmlList += `<span>${item.ed}</span>`;
+//       }
+//       htmlList += '</li>';
+//     });
+//     htmlList += '</ul>';
 
-    // Send the HTML as the response
-    res.end(htmlList);
-  });
-})
+//     // Send the HTML as the response
+//     res.end(htmlList);
+//   });
+// })
 
 
   app.get("/logout", (req, res) => {
